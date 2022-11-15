7ab9ab765341b689cd59381d4c1c9eaebf315bc0
YakovkinVD <dikobrazzzik709@gmail.com>
2022-11-16 00:15:31 +0900

    Task_64,66 solved

---
+++ Task_64/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 64: Задайте значение N. Напишите программу, которая 
+выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1" 
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+System.Console.Clear();
+
+// ввод значений с клавиатуры
+string ToInputVar(string input_text)
+{
+    Console.Write($"Введите {input_text} и нажмите Enter: ");
+    string result = Console.ReadLine();
+    return result;
+}
+//обратный отсчет
+string FinalCountdown(int n)
+{   if (n<1) 
+    {Console.WriteLine("BOOM!");
+    return "__Game over__";
+    }
+    Console.Write($"{n}..");
+    n--;
+    return FinalCountdown(n);
+}
+
+string input_text = "натуральное число N";
+int n = int.Parse(ToInputVar(input_text));
+Console.Write(FinalCountdown(n));
---
+++ Task_66/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт
+ сумму натуральных элементов в промежутке от M до N. С помощью рекурсии!!!
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+// ввод значений с клавиатуры
+Console.Clear();
+string ToInputVar(string input_text)
+{
+    Console.Write($"Введите {input_text} и нажмите Enter: ");
+    string result = Console.ReadLine();
+    return result;
+}
+//замена значений переменных местами
+static void ToSwap(ref int oldvar, ref int newvar)
+{
+    int temp = oldvar;
+    oldvar = newvar;
+    newvar = temp;
+}
+
+//найти сумму чисел между n и m
+int ToFindSumBetweenMN(int n, int m,int sum=0)
+{
+    if (n <= m)
+    {
+        sum=sum+n;
+        n++;
+        return ToFindSumBetweenMN(n, m,sum);        
+
+    }
+    else if (n == m+1)
+    {
+        return sum;
+    }
+    else 
+    {
+        ToSwap(ref m, ref n);
+        return ToFindSumBetweenMN(n, m,sum);
+    }
+}
+
+
+string input_text = "натуральное число N";
+int n = int.Parse(ToInputVar(input_text));
+input_text = "натуральное число M";
+int m = int.Parse(ToInputVar(input_text));
+Console.WriteLine($"Сумма чисел между {n} и {m} равна " + ToFindSumBetweenMN(n, m));
